849ad60c0f61eaed77f67670e69b8275b1ef6424
gram <git@orsinium.dev>
2022-11-24 11:52:15 +0100

    test custom middlewares

--- tests/test_actors.py
+++ tests/test_actors.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import asyncio
 import time
 from contextlib import contextmanager
-
+from .helpers import get_random_name
 import pytest
 
 import walnats
@@ -15,8 +15,14 @@ async def run_burst(
     **kwargs,
 ) -> None:
     events_names = set()
-    events = []
+    events: list[walnats.types.BaseEvent] = []
+    e: walnats.types.BaseEvent
     for e, _ in messages:
+        if e.name not in events_names:
+            events.append(e)
+            events_names.add(e.name)
+    for a in actors:
+        e = a.event
         if e.name not in events_names:
             events.append(e)
             events_names.add(e.name)
@@ -33,11 +39,11 @@ async def run_burst(
 
 
 @contextmanager
-def faster_than(max_dur: float):
+def duration_between(min_dur: float, max_dur: float):
     start = time.perf_counter()
     yield
     actual_dur = time.perf_counter() - start
-    assert actual_dur < max_dur
+    assert min_dur <= actual_dur < max_dur
 
 
 @pytest.mark.asyncio
@@ -49,11 +55,44 @@ async def test_many_messages_one_event() -> None:
         await asyncio.sleep(.1)
         received.append(e)
 
-    e = walnats.Event('event', str)
-    with faster_than(.3):
+    e = walnats.Event(get_random_name(), str)
+    with duration_between(0, .3):
         await run_burst(
-            walnats.Actor('handler', e, handler),
+            walnats.Actor(get_random_name(), e, handler),
             messages=[(e, m) for m in messages],
-            batch=20,
+            batch=len(messages),
         )
     assert set(received) == set(messages)
+
+
+@pytest.mark.asyncio
+async def test_respect_timeout() -> None:
+    async def handler(e: str) -> None:
+        raise AssertionError('unreachable')
+
+    e = walnats.Event(get_random_name(), str)
+    with duration_between(.1, .15):
+        await run_burst(
+            walnats.Actor(get_random_name(), e, handler),
+            messages=[],
+            poll_delay=.1,
+        )
+
+
+def test_actors_get():
+    async def noop(_):
+        pass
+
+    e = walnats.Event(get_random_name(), str)
+    actors = walnats.Actors(
+        walnats.Actor('a1', e, noop),
+        walnats.Actor('a2', e, noop),
+        walnats.Actor('a3', e, noop),
+    )
+    a1 = actors.get('a1')
+    assert a1
+    assert a1.name == 'a1'
+    a2 = actors.get('a2')
+    assert a2
+    assert a2.name == 'a2'
+    assert actors.get('something') is None
---
+++ tests/test_middlewares.py
@@ -0,0 +1,125 @@
+from __future__ import annotations
+
+import asyncio
+from .helpers import get_random_name
+import pytest
+
+import walnats
+
+
+async def run_actor(
+    actor: walnats.Actor[str, None],
+    message: str,
+    **kwargs,
+) -> None:
+    assert isinstance(actor.event, walnats.Event)
+    events_reg = walnats.Events(actor.event)
+    actors_reg = walnats.Actors(actor)
+    async with events_reg.connect() as pub_conn, actors_reg.connect() as sub_conn:
+        await pub_conn.register()
+        await sub_conn.register()
+        await asyncio.gather(
+            sub_conn.listen(burst=True, **kwargs),
+            pub_conn.emit(actor.event, message),
+        )
+
+
+@pytest.mark.asyncio
+async def test_custom_sync() -> None:
+    triggered = []
+
+    class Middleware(walnats.middlewares.BaseSyncMiddleware):
+        def on_failure(self, ctx: walnats.types.ErrorContext) -> None:
+            triggered.append('on_failure')
+
+        def on_start(self, ctx: walnats.types.Context) -> None:
+            triggered.append('on_start')
+
+        def on_success(self, ctx: walnats.types.OkContext) -> None:
+            triggered.append('on_success')
+
+    async def handler(msg: str) -> None:
+        assert msg == 'hi'
+        triggered.append('handler')
+
+    e = walnats.Event(get_random_name(), str)
+    a = walnats.Actor(get_random_name(), e, handler, sync_middlewares=(Middleware(),))
+    await run_actor(a, 'hi')
+    assert triggered == ['on_start', 'handler', 'on_success']
+
+
+@pytest.mark.asyncio
+async def test_custom_sync__on_failure() -> None:
+    triggered = []
+
+    class Middleware(walnats.middlewares.BaseSyncMiddleware):
+        def on_failure(self, ctx: walnats.types.ErrorContext) -> None:
+            triggered.append('on_failure')
+
+        def on_start(self, ctx: walnats.types.Context) -> None:
+            triggered.append('on_start')
+
+        def on_success(self, ctx: walnats.types.OkContext) -> None:
+            triggered.append('on_success')
+
+    async def handler(msg: str) -> None:
+        assert msg == 'hi'
+        triggered.append('handler')
+        raise ZeroDivisionError
+
+    e = walnats.Event(get_random_name(), str)
+    a = walnats.Actor(get_random_name(), e, handler, sync_middlewares=(Middleware(),))
+    await run_actor(a, 'hi')
+    assert triggered == ['on_start', 'handler', 'on_failure']
+
+
+@pytest.mark.asyncio
+async def test_custom_async() -> None:
+    triggered = []
+
+    class Middleware(walnats.middlewares.BaseAsyncMiddleware):
+        async def on_failure(self, ctx: walnats.types.ErrorContext) -> None:
+            triggered.append('on_failure')
+            raise ctx.exception
+
+        async def on_start(self, ctx: walnats.types.Context) -> None:
+            triggered.append('on_start')
+
+        async def on_success(self, ctx: walnats.types.OkContext) -> None:
+            triggered.append('on_success')
+
+    async def handler(msg: str) -> None:
+        assert msg == 'hi'
+        triggered.append('handler')
+
+    e = walnats.Event(get_random_name(), str)
+    a = walnats.Actor(get_random_name(), e, handler, async_middlewares=(Middleware(),))
+    await run_actor(a, 'hi')
+    assert len(triggered) == 3
+    assert set(triggered) == {'on_start', 'handler', 'on_success'}
+
+
+@pytest.mark.asyncio
+async def test_custom_async__on_failure() -> None:
+    triggered = []
+
+    class Middleware(walnats.middlewares.BaseAsyncMiddleware):
+        async def on_failure(self, ctx: walnats.types.ErrorContext) -> None:
+            triggered.append('on_failure')
+
+        async def on_start(self, ctx: walnats.types.Context) -> None:
+            triggered.append('on_start')
+
+        async def on_success(self, ctx: walnats.types.OkContext) -> None:
+            triggered.append('on_success')
+
+    async def handler(msg: str) -> None:
+        assert msg == 'hi'
+        triggered.append('handler')
+        raise ZeroDivisionError
+
+    e = walnats.Event(get_random_name(), str)
+    a = walnats.Actor(get_random_name(), e, handler, async_middlewares=(Middleware(),))
+    await run_actor(a, 'hi')
+    assert len(triggered) == 3
+    assert set(triggered) == {'on_start', 'handler', 'on_failure'}
